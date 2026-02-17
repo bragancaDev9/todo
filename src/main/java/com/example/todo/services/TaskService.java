d7090c9f629a6f496366092d22c649af1e4038fe
bragancaDev9 <bryanstos99@gmail.com>
2026-02-17 20:58:11 -0300

    Exception handling, operation findById()

--- src/main/java/com/example/todo/services/TaskService.java
+++ src/main/java/com/example/todo/services/TaskService.java
@@ -1,12 +1,14 @@
 package com.example.todo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.todo.entities.Task;
 import com.example.todo.repositories.TaskRepository;
+import com.example.todo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TaskService {
@@ -15,5 +17,11 @@ public class TaskService {
 	
 	public List<Task> findAll() {	
 		return taskRepository.findAll();
+	}
+	
+	public Task findById(String id) {
+		Optional<Task> task = taskRepository.findById(id);
+		
+		return task.orElseThrow(() -> new ObjectNotFoundException("Object not found! Id: " + id));
 	}
 }
